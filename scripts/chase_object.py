e8e51406b80a0fba5affb68dc8f33d1b93f526e5
Pranay Mathur <37473401+Matnay@users.noreply.github.com>
2023-02-17 15:33:33 -0500

    Update chase_object.py

--- scripts/chase_object.py
+++ scripts/chase_object.py
@@ -4,7 +4,7 @@
 
 import rclpy
 from rclpy.node import Node
-from geometry_msgs.msg import Pose2D, Twist
+from geometry_msgs.msg import Point,Twist
 from rclpy.qos import QoSProfile, QoSReliabilityPolicy, QoSHistoryPolicy, QoSDurabilityPolicy
 
 import sys
@@ -20,36 +20,49 @@ class chase_object(Node):
             durability=QoSDurabilityPolicy.RMW_QOS_POLICY_DURABILITY_VOLATILE,
             depth=1
         )
-		# Declare that the rotate_robot node is subcribing to the /camera/image/compressed topic.
-		self.angle_subscriber = self.create_subscription(
-				Pose2D, '/angle', self.object_pose_callback, qos_profile)
+		# Declare that the chase_object node is subcribing to the /camera/image/compressed topic.
+		self.point_subscriber = self.create_subscription(
+				Point, '/LidarCameraCoordinates', self.point_callback, qos_profile)
 
 		self.velocity_publisher = self.create_publisher(Twist, 'cmd_vel', 10)
 		
 	def control_rotation(self):
 		velocity_msg = Twist()
 				
-		Kp = 1.25	# Define Proportional Controller Gain
-		reference_point = 0.5427975	#Define Center as the reference point
+		Kp_angular = 0.005	# Define Proportional Controller Gain
+		reference_point = 160	#Define Center as the reference point
+		Kp_forward = 0.1
 
-		e = self.theta - reference_point
-		velocity_msg.angular.z = -Kp * e
+		e = int(self.cx) - reference_point
+		e_forward = self.distance_y-0.4
 
+		if abs(e)>10:
+			velocity_msg.angular.z = -Kp_angular * e
+			print("Angular Velocity:",velocity_msg.angular.z)
+		if abs(e_forward)>0.1:
+			velocity_msg.linear.x = Kp_forward * e_forward
+			print("Linear Velocity:",velocity_msg.linear.x)
+		elif abs(e_forward)>2:
+			velocity_msg.linear.x = 0.4
+			#print(self.distance_y)
+		else:
+			velocity_msg.linear.x=0.0
+			velocity_msg.angular.z=0.0
 		self.velocity_publisher.publish(velocity_msg)
 		
-
-	def object_pose_callback(self, msg):	
-		self.theta = msg.theta
+	def point_callback(self, msg):	
+		self.cx = msg.x
+		self.distance_y = msg.y
 		self.control_rotation()
 
 
 def main():
 	rclpy.init() #init routine needed for ROS2.
-	object_pose_subscriber = chase_object() #Create class object to be used.
-	rclpy.spin(object_pose_subscriber) # Trigger callback processing.		
+	point_subscriber = chase_object() #Create class object to be used.
+	rclpy.spin(point_subscriber) # Trigger callback processing.		
 
 	#Clean up and shutdown.
-	object_pose_subscriber.destroy_node()  
+	point_subscriber.destroy_node()  
 	rclpy.shutdown()
 
 
