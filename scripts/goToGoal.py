09194c545539fc5904eacc843707849b396b5250
Pranay Mathur <37473401+Matnay@users.noreply.github.com>
2023-03-06 15:09:33 -0500

    Update goToGoal.py

--- scripts/goToGoal.py
+++ scripts/goToGoal.py
@@ -7,10 +7,11 @@ from rclpy.node import Node
 from geometry_msgs.msg import Point,Twist
 from rclpy.qos import QoSProfile, QoSReliabilityPolicy, QoSHistoryPolicy, QoSDurabilityPolicy
 from nav_msgs.msg import Odometry
-
+from sensor_msgs.msg import LaserScan
+from geometry_msgs.msg import Pose2D
 import sys
 import numpy as np
-
+import time
 class chase_object(Node):
 
     def __init__(self):
@@ -24,8 +25,11 @@ class chase_object(Node):
         )
         # Declare that the chase_object node is subcribing to the /camera/image/compressed topic.
         self.odom_subscriber = self.create_subscription(Odometry, '/odom', self.odom_callback, qos_profile)
+        self.scan_subscriber = self.create_subscription(Pose2D, '/object_pose', self.pose_callback, qos_profile)
         self.velocity_publisher = self.create_publisher(Twist, '/cmd_vel', 10)
         self.waypoints = []
+        self.laserscan_subscriber = self.create_subscription(
+            LaserScan,'/scan',self.scan_callback,qos_profile)
         with open("wayPoints.txt") as file_in:
             for line in file_in:
                 self.waypoints.append((float(line.split()[0]),float(line.split()[1])))
@@ -35,7 +39,19 @@ class chase_object(Node):
         self.waypoint_count=0
         self.Init = True
         self.odom=Odometry()
- 
+        self.average_distance=1.5
+        self.laserflag=True
+
+    def scan_callback(self, msg):	
+        range_data=LaserScan()
+        range_data.ranges=msg.ranges
+        #print(len(msg.ranges))
+        #self.average_distance=np.mean([range_data.ranges[-1],range_data.ranges[-2],range_data.ranges[-3],range_data.ranges[0],range_data.ranges[1],range_data.ranges[2],range_data.ranges[3]])		
+        self.average_distance=range_data.ranges[0]
+        #print(self.average_distance)
+    
+    def pose_callback(self,msg):
+        self.obstacle_detcted=msg
 
     def waypoint_traversal(self):
         if(self.reached_goal):
@@ -44,16 +60,17 @@ class chase_object(Node):
             self.simple_velocity_controller(0.0,0.0)
             print(self.waypoints[self.waypoint_count][0],self.waypoints[self.waypoint_count][1])
         
-        x=self.waypoints[self.waypoint_count][0]+0.1
+        x=self.waypoints[self.waypoint_count][0]+0.2
         y=self.waypoints[self.waypoint_count][1]
         
         if(self.waypoint_count==0):
             if(abs(self.globalPos_x-x)>0.06):
-                self.simple_velocity_controller(0.15,0.0)
+                self.simple_velocity_controller(0.19,0.0)
                 print(self.globalPos_x-x)
-            elif(abs(self.globalPos_x-x)<0.1 and ((self.globalAng-1.57)>6.1 or (self.globalAng-1.57)<0)):
-                print(self.globalAng-1.57)
-                self.simple_velocity_controller(0.0,0.1)
+                print("Angle:",self.globalAng)
+            elif(abs(self.globalAng)<1.57):
+                print("Angle:",abs(self.globalAng-1.57))
+                self.simple_velocity_controller(0.0,0.8)
             else:
                 self.simple_velocity_controller(0.0,0.0)
                 print("Reached Goal 0!!")   
@@ -62,15 +79,80 @@ class chase_object(Node):
             # when y is around 1 turn to heading 3.14 move till x is 0.2, turn till angle less than 1.5 go till y is less than 0.5 
             # turn till angle becomes negative go til x becomes gt 0 turn till angle is gt 1.57 
             # go till y becomes greater than 0
-            if(abs(self.globalPos_y-y)>0.01):
-                self.simple_velocity_controller(0.15,0.0)
+            if(self.average_distance<0.25 and self.laserflag):
+                #Move in a rectangle
+                self.laserflag=False
+                self.simple_velocity_controller(0.0,1.0)
+                print("turn left")
+                time.sleep(1.8)
+                self.simple_velocity_controller(0.18,0.0)
+                time.sleep(2)
+                print("go straight")
+                self.simple_velocity_controller(0.0,-1.0)
+                time.sleep(2)
+                print("turn right")
+                self.simple_velocity_controller(0.18,0.0)
+                time.sleep(4)
+                print("go straight")
+                self.simple_velocity_controller(0.0,-1.0)
+                time.sleep(2)
+                print("turn right")
+                self.simple_velocity_controller(0.18,0.0)
+                time.sleep(2)
+                print("go straight")
+                self.simple_velocity_controller(0.0,1.0)
+                print("turn left")
+                time.sleep(2)
+            if(abs(self.globalPos_y-y)>0.1):
+                print("x",self.globalPos_x-x)
+                print("y",self.globalPos_y-y) 
+                print("ang",self.globalAng)
+                self.simple_velocity_controller(0.18,0.0)
             else:
                 self.simple_velocity_controller(0.0,0.0)
-                print("Reached Goal 1!!")  
-            print("x",self.globalPos_x-x)
-            print("y",self.globalPos_y-y) 
-            print("ang",self.globalAng)
+                print("Reached Goal 1!!")
+                self.reached_goal=True
+                self.laserflag=True
+                self.simple_velocity_controller(0.0,1.0)
+                print("turn left")
+                time.sleep(1.6)
 
+        if(self.waypoint_count==2):
+            # when y is around 1 turn to heading 3.14 move till x is 0.2, turn till angle less than 1.5 go till y is less than 0.5 
+            # turn till angle becomes negative go til x becomes gt 0 turn till angle is gt 1.57 
+            # go till y becomes greater than 0
+            if(self.average_distance<0.3 and self.laserflag):
+                #Move in a rectangle
+                self.laserflag=False
+                self.simple_velocity_controller(0.0,1.0)
+                print("turn left")
+                time.sleep(1.8)
+                self.simple_velocity_controller(0.18,0.0)
+                time.sleep(3)
+                print("go straight")
+                self.simple_velocity_controller(0.0,-1.0)
+                time.sleep(2)
+                print("turn right")
+                self.simple_velocity_controller(0.18,0.0)
+                time.sleep(4)
+                print("go straight")
+                self.simple_velocity_controller(0.0,-1.0)
+                time.sleep(1.8)
+                print("turn right")
+                self.simple_velocity_controller(0.18,0.0)
+                time.sleep(3)
+                print("go straight")
+                self.simple_velocity_controller(0.0,1.0)
+                print("turn left")
+                time.sleep(2)
+            elif(abs(self.globalPos_x-x)>0.1):
+                print("x",self.globalPos_x-x)
+                #print("y",self.globalPos_y-y) 
+                self.simple_velocity_controller(0.18,0.0)
+            else:
+                self.simple_velocity_controller(0.0,0.0)
+                print("Reached Goal 2!!")
+                self.reached_goal=True
 
     def simple_velocity_controller(self,error_x,error_angular):
         velocity_msg = Twist()
