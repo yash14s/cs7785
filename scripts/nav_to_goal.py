7b06697f25a539a8787ef4d4a51de5a01fd5c87c
Yash <yash33srivastava@gmail.com>
2023-04-06 20:20:16 -0400

    modified:   scripts/nav_to_goal.py

--- scripts/nav_to_goal.py
+++ scripts/nav_to_goal.py
@@ -6,7 +6,7 @@ import rclpy
 from rclpy.node import Node
 from rclpy.qos import QoSProfile, QoSReliabilityPolicy, QoSHistoryPolicy, QoSDurabilityPolicy
 from nav_msgs.msg import Odometry
-from geometry_msgs.msg import Pose2D, PoseStamped,PoseWithCovariance
+from geometry_msgs.msg import Pose2D, PoseStamped, PoseWithCovarianceStamped
 import sys
 import numpy as np
 import time
@@ -22,7 +22,8 @@ class goToGoal(Node):
             depth=1
         )
         # Declare that the goToGoal node is subcribing to the /camera/image/compressed topic.
-        self.odom_subscriber = self.create_subscription(PoseWithCovariance, '/amcl_pose', self.pose_callback, qos_profile)
+        self.odom_subscriber = self.create_subscription(PoseWithCovarianceStamped, '/amcl_pose', self.pose_callback, qos_profile)
+
         self.goal_publisher = self.create_publisher(PoseStamped, '/goal_pose', 10)
  
         self.waypoints = []
@@ -34,14 +35,28 @@ class goToGoal(Node):
         with open(wp_file) as file_in:
             for line in file_in:
                 self.waypoints.append((float(line.split()[0]),float(line.split()[1])))
-        self.robot_pose = PoseWithCovariance()
+        #self.robot_pose = PoseWithCovariance()
         self.publish_goals()
- 
-    def pose_callback(self,msg):
-        self.robot_pose=msg
+
+
+    def pose_callback(self, msg):
+        #self.robot_pose = PoseWithCovarianceStamped()
+        print(msg)
+        self.robot_pose = msg
+
+        euclidean_dist=((self.robot_pose.pose.position.x-self.waypoints[self.waypoint_count][0])**2+(self.robot_pose.pose.position.y-self.waypoints[self.waypoint_count][1])**2)**0.5
+        if(euclidean_dist<0.1):
+            print("goal_reached")
+            self.waypoint_count+=1
+            if(self.waypoint_count!=len(self.waypoints-1)):
+                self.publish_goals()
+            else:
+                print("completed")
+                exit(0)
+        
  
     def publish_goals(self):
-        print("New Goal Reached")
+        print("Publish goals")
         goal = PoseStamped()
         t = self.get_clock().now()
         goal.header.stamp=t.to_msg()
@@ -55,17 +70,9 @@ class goToGoal(Node):
         goal.pose.orientation.z=0.0
         self.goal_publisher.publish(goal)
         
-        euclidean_dist=((self.robot_pose.pose.position.x-self.waypoints[self.waypoint_count][0])**2+(self.robot_pose.pose.position.y-self.waypoints[self.waypoint_count][1])**2)**0.5
-        if(euclidean_dist<0.1):
-            print("goal_reached")
-            self.waypoint_count+=1
-            if(self.waypoint_count!=len(self.waypoints-1)):
-                self.publish_goals()
-            else:
-                print("completed")
-                exit(0)
-        else:
-            self.publish_goals()
+        
+        #else:
+            #self.publish_goals()
  
             ## replace this with feedback that you reached, Take x,y from self.robot pose compare with goal x y
  
